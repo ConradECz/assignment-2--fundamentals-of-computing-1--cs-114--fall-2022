b28c89b1fdc72cf61459fc4bb00791221c851676
ConradECz <conradec21@gmail.com>
2022-10-02 21:01:44 -0400

    Declared variables, implemented for loops with incrementes upon user input

--- Assignment2.java
+++ Assignment2.java
@@ -1,5 +1,33 @@
 public class Assignment2 {
+    import java.util.Scanner
     public static void main(String[] args) {
-        System.out.println("test");
+
+        int = x;
+        int = y;
+        int = rows;
+
+        static Scanner diamond = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        rows = diamond.nextInt();
+
+        for(x = 0; x <= rows; x++){
+            for(y = 1; y <= rows - x; y++){
+                System.out.print(" ");
+            }
+            for(y = 1; y <= 2*i-1; y++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(x = rows - 1; x >=1; i--){
+            for(y = 1; y <= rows - x; y++){
+                System.out.print(" ");
+            }
+            for(y = 1; y <= 2*i-1; y++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
