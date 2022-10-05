5c2042164bca73b6d98ea8cf19e611eaa689e7a4
ConradECz <conradec21@gmail.com>
2022-10-04 21:36:11 -0400

    Updated user input for both cases

--- Assignment2.java
+++ Assignment2.java
@@ -7,7 +7,7 @@ public class Assignment2 {
         System.out.println("Enter a number:");
             int rows = diamond.nextInt();
 
-    if(rows%2 > 0){
+    if(rows % 2 > 0){
         for(int i = 1; i <= rows / 2 + 1; i++){
             for(int x = 0; x <= rows - 1 - i; x++){
                 System.out.print(" ");
@@ -26,7 +26,7 @@ public class Assignment2 {
             }
                 System.out.println("");
             }
-        } else {
+    }else{
         for(int i = 1; i <= rows / 2 + 1; i++){
             if(i == 1){
                 for(int x = 2; x < rows - i; x++){
@@ -35,15 +35,15 @@ public class Assignment2 {
                     System.out.print(" * ");
                     System.out.println("");
             }else{
-            for(int x = 1; x < rows - i; x++){
-                System.out.print("  ");
+                for(int x = 1; x < rows - i; x++){
+                    System.out.print("  ");
+                }
+                for(int x = 2; x < i * 2; x++){
+                    System.out.print("* ");
+                }
+                    System.out.println("");
+                }
             }
-            for(int x = 2; x < i * 2; x++){
-                System.out.print("* ");
-            }
-                System.out.println("");
-            }
-        }
         for(int i = rows / 2; i >= 1; i--){
             if(i == 1){
                 for(int x = 2; x < rows - i; x++){
@@ -52,15 +52,15 @@ public class Assignment2 {
                     System.out.print(" * ");
                     System.out.println("");
             }else{
-            for(int x = 2; x <= rows - i; x++){
-                System.out.print("  ");
+                for(int x = 2; x <= rows - i; x++){
+                    System.out.print("  ");
+                }
+                for(int x = 2; x < i * 2; x++){
+                    System.out.print("* ");
+                }
+                    System.out.println("");
+                }
             }
-            for(int x = 2; x < i * 2; x++){
-                System.out.print("* ");
-            }
-                System.out.println("");
         }
-        }
-    }
     }
 }
